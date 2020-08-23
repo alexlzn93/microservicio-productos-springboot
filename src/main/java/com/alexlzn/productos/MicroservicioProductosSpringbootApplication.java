09861e41ebd4e759b9c92853aeb6e8bcdaf11715
alexlzn93 <alex93lzn@gmail.com>
2020-08-23 10:27:52 +0200

    Initial commit

---
+++ src/main/java/com/alexlzn/productos/MicroservicioProductosSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.alexlzn.productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioProductosSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioProductosSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexlzn/productos/MicroservicioProductosSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexlzn.productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioProductosSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
