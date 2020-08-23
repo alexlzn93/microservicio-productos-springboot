6f1e1e0118b5e46ac19d505df6f655c02e81ac60
alexlzn93 <alex93lzn@gmail.com>
2020-08-23 11:48:22 +0200

    Create import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO productos (nombre, precio, fecha) 	VALUES ('Samsung Smart TV', 699, NOW());
+INSERT INTO productos (nombre, precio, fecha) 	VALUES ('Asus RogStrix', 1200, NOW());
+INSERT INTO productos (nombre, precio, fecha) 	VALUES ('Apple TV', 299, NOW());
+INSERT INTO productos (nombre, precio, fecha) 	VALUES ('Garmin SmartWatch', 699, NOW());
+INSERT INTO productos (nombre, precio, fecha) 	VALUES ('Iphone 11', 899, NOW());
+INSERT INTO productos (nombre, precio, fecha) 	VALUES ('Lavadora AEG', 569, NOW());
