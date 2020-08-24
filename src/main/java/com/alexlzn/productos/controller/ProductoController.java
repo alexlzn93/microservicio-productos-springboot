c28a05e609450ccda5ed3f98f86ca7e986964552
alexlzn93 <alex93lzn@gmail.com>
2020-08-24 12:57:01 +0200

    añado @Value y pruebas con enviroment

--- src/main/java/com/alexlzn/productos/controller/ProductoController.java
+++ src/main/java/com/alexlzn/productos/controller/ProductoController.java
@@ -1,10 +1,15 @@
 package com.alexlzn.productos.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alexlzn.productos.interfaces.IProductoService;
@@ -15,17 +20,34 @@ public class ProductoController {
 	
 	@Autowired
 	IProductoService productservice;
+	@Autowired
+	private Environment enviroment;
 	
+	@Value("${server.port}") //value para inyectar a Spring valores que definamos en el application.properties
+	private Integer port;
 	
 	@GetMapping("/listProductos")
 	public List<Producto> getListProducto(){
-		return productservice.findAll();
+		return productservice.findAll().stream().map(producto-> {
+			//producto.setPort(Integer.parseInt(enviroment.getProperty("local.server.port")));
+			producto.setPort(port);
+			return producto;
+		}).collect(Collectors.toList());
 	}
 	
 	@GetMapping("/productoID/{id}")
 	public Producto findByIdProducto(@PathVariable("id") Long id) {
 		System.out.println("Producto con id " +id);
-		return productservice.findById(id);
+		Producto producto= productservice.findById(id);
+		 //se pone local y el nombre de la variable definida en el application.properties (server.port)
+		//producto.setPort(Integer.parseInt(enviroment.getProperty("local.server.port")));
+		producto.setPort(port);
+		return producto;
+	}
+	@PostMapping("/nuevoProducto")
+	public Producto nuevoProducto(@RequestBody Producto producto ) {
+		productservice.guardarProducto(producto);
+		return producto;
 	}
 
 }
